ff182bca377c8350c3a4e286e5b1b4c25e682858
MeiLin <postget.me@gmail.com>
2015-01-07 23:42:32 +0800

    refactoring

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -4,17 +4,8 @@ var createMiniHarp = require("mini-harp")
   , app = createMiniHarp();
 
 var argv = parseArgs(process.argv.slice(2));
-var port = argv.port || "4000";
-
-app.use(function (req, res, next) {
-	if (req.url == "/current-time") {
-		res.end((new Date()).toISOString());
-	}
-	else {
-		next();
-	}
-});
+var port = argv.port || 4000;
 
 console.log("Starting mini-harp on http://localhost:" + port);
-app.listen(parseInt(port));
+app.listen(port);
 
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var connect = require('connect');
 
-module.exports = connect;
+module.exports = createServer;
 
+function createServer() {
+	var app = connect();
+
+	app.use(function (req, res, next) {
+		if (req.url == "/current-time") {
+			res.end((new Date()).toISOString() + '\n');
+		}
+		else {
+			next();
+		}
+	});
+
+	return app;
+}

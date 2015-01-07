335654f1d125b172d5df3941bf2bf2754c9d7507
MeiLin <postget.me@gmail.com>
2015-01-07 21:50:59 +0800

    parse --port

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-
+var parseArgs = require("minimist");
 var createMiniHarp = require("mini-harp")
   , app = createMiniHarp();
-console.log("Starting mini-harp on http://localhost:4000");
-app.listen(4000);
+
+var argv = parseArgs(process.argv.slice(2));
+var port = argv.port || "4000";
+
+console.log("Starting mini-harp on http://localhost:" + port);
+app.listen(parseInt(port));
 
